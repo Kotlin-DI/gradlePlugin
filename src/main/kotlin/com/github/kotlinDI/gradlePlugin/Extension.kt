4da92347d0ef95409b8ec7692fc3e2dbc3911ce6
Ivan Molodtsov <ivan.molodtsov@lunatech.nl>
2023-04-14 18:15:36 +0200

    update versioning

--- src/main/kotlin/com/github/kotlinDI/gradlePlugin/Extension.kt
+++ src/main/kotlin/com/github/kotlinDI/gradlePlugin/Extension.kt
@@ -3,6 +3,8 @@ package com.github.kotlinDI.gradlePlugin
 import org.gradle.api.provider.Property
 
 interface Extension {
+    val common: Property<String>
+    val processor: Property<String>
     val keysFile: Property<String>
     val pluginFile: Property<String>
 }
